44a6f09adeda85343fd54b43acee6bf5fcd6c1b8
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-11-29 18:10:48 +1100

    Implements key vault and github testing
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,40 +1,11 @@
-
-# Resource Group Variables
 variable "resource_group_name" {
   type        = string
-  description = "Existing resource group where the IKS cluster will be provisioned."
-}
-
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "The api key for IBM Cloud access"
+  description = "Name of the network resource group."
 }
 
 variable "region" {
   type        = string
-  description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
-}
-
-variable "namespace" {
-  type        = string
-  description = "Namespace for tools"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "The name of the cluster"
-  default     = ""
-}
-
-variable "cluster_type" {
-  type        = string
-  description = "The type of cluster that should be created (openshift or kubernetes)"
-}
-
-variable "cluster_exists" {
-  type        = string
-  description = "Flag indicating if the cluster already exists (true or false)"
-  default     = "true"
+  description = "Azure region/location to deploy resources."
 }
 
 variable "name_prefix" {
@@ -43,8 +14,22 @@ variable "name_prefix" {
   default     = ""
 }
 
-variable "vpc_cluster" {
-  type        = bool
-  description = "Flag indicating that this is a vpc cluster"
-  default     = false
+variable "subscription_id" {
+  type = string
+  default = null
+}
+
+variable "client_id" {
+  type = string
+  default = null
+}
+
+variable "client_secret" {
+  type = string
+  default = null
+}
+
+variable "tenant_id" {
+  type = string
+  default = null
 }
